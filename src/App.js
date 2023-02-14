34214da1e65222055b600560b01573eb29550f78
simmeringstew <dev.95a2e@simplelogin.com>
2023-02-14 15:34:18 -0500

    Added area for the job listings to go

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { useState } from "react";
+import data from "./data.json";
+import Job from "./components/Job";
 import './App.css';
 
 const App = () => {
+
+  const [jobs, setJobs] = useState([...data]);
+
   return(
     <div>
       <div className="top-design" aria-hidden="true"></div>
+      <main className="container px-5">
+        {jobs.map(job =>
+        <Job key={job.id} job={job} />
+        )}
+      </main>
     </div>
   );
 }
